ae0d0a8bf60bbae741b6aedc1c794eb3912df16f
dungnguyen1991 <dungnguyendeveloper1991@gmail.com>
2021-11-01 13:26:44 +0700

    update wait_for_db command for the new Django version

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,7 @@
 import time
 
-from django.db import connections
+from psycopg2 import OperationalError as Psycopg20pError
+
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
@@ -10,11 +11,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('waiting for database...')
-        db_conn = None
-        while not db_conn:
+        db_up = False
+        while db_up is False:
             try:
-                db_conn = connections['default']
-            except OperationalError:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg20pError, OperationalError):
                 self.stdout.write('Database unavialable, waiting 1 second...')
                 time.sleep(1)
 
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,5 +1,7 @@
 from unittest.mock import patch
 
+from psycopg2 import OperationalError as Psycopg20pError
+
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import TestCase
@@ -9,15 +11,17 @@ class CommandTests(TestCase):
 
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
-            gi.return_value = True
+        with patch('django.core.management.base.BaseCommand.check') as ch:
+            ch.return_value = True
             call_command('wait_for_db')
-            self.assertEqual(gi.call_count, 1)
+            self.assertEqual(ch.call_count, 1)
 
     @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
-            gi.side_effect = [OperationalError] * 5 + [True]
+        with patch('django.core.management.base.BaseCommand.check') as ch:
+            ch.side_effect = ([OperationalError] * 5 +
+                              [Psycopg20pError]*2 +
+                              [True])
             call_command('wait_for_db')
-            self.assertEqual(gi.call_count, 6)
+            self.assertEqual(ch.call_count, 8)
